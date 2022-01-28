7086b3d09c18e946da5f7787702ec23da0cdbf3c
Tik88 <xtikx@yandex.ru>
2022-01-28 11:19:22 +0300

    Добавьте файлы проекта.

---
+++ RevitAPI_Lab2.1/Main.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI_Lab2._1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var vents = new FilteredElementCollector(doc)
+            .OfClass(typeof(FilledRegion))
+            .Cast<FilledRegion>()
+            .ToList();
+
+
+            TaskDialog.Show("Количество воздуховодов", vents.Count.ToString());
+
+            return Result.Succeeded;
+        }
+    }
+}
